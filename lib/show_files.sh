c01c8b8fc92efdbdd99ba23fb9fbe704d0f036d7
Eduardo Cardoso <eduardo@Eduardos-MacBook-Pro.local>
2018-06-18 23:29:52 +0100

    fix generate file

--- lib/show_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pwd 
-ls
-ls sb-sample/build/libs/*
--- update-pipeline.sh
+++ update-pipeline.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-fly -t tutorial set-pipeline -c pipeline.yml -p springboot-sample
-fly -t tutorial up -p springboot-sample
+fly -t tutorial set-pipeline -c pipeline.yml -p springboot-sample-v1
+fly -t tutorial up -p springboot-sample-v1
